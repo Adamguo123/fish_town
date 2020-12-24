786baf5f9592d99922e1b4fa56486b98d3c98dae
guowang <guowang@aerozhonghuan.com>
2020-12-24 15:37:59 +0800

    project init

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
+fastClick.attach(document.body)
 
 createApp(App).mount('#app')
